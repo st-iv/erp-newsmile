3df7ea50c1ce5893f13219024c087fe3b10e661a
ryzhiy <nikrj93@gmail.com>
2018-08-07 13:26:27 +0800

    #add статусы зубов

---
+++ local/modules/mmit.newsmile/lib/status/tooth.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niki_
+ * Date: 30.05.2018
+ * Time: 16:19
+ */
+namespace Mmit\NewSmile\Status;
+
+use Bitrix\Main\Entity;
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Type\Date;
+use Mmit\NewSmile\Status;
+
+Loc::loadMessages(__FILE__);
+
+class ToothTable extends Status
+{
+    public static function getTableName()
+    {
+        return 'm_newsmile_status_tooth';
+    }
+}
