9abb43dc5bc8b0e5923215428542225209e1ca99
Alexander Bondarev <al94.bondarev@gmail.com>
2018-09-21 15:20:34 +0800

    #change перевод прейскуранта на компонент entity

--- price-list/index.php
+++ price-list/index.php
@@ -1,12 +1,24 @@
 <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Новый раздел");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Прейскурант");
 ?>
 
-<?$APPLICATION->IncludeComponent('newSmile:pricelist.list',
-    [
-
-    ]
+<?$APPLICATION->IncludeComponent(
+    "newSmile:entity",
+    "",
+    Array(
+        'DATA_MANAGER_CLASS_ELEMENT' => 'Mmit\NewSmile\Service\ServiceTable',
+        'DATA_MANAGER_CLASS_GROUP' => 'Mmit\NewSmile\Service\GroupTable',
+        'SEF_MODE' => 'Y',
+        'SEF_FOLDER' => '/price-list/',
+        'SECTION_ID' => $arResult['VARIABLES']['SECTION_ID'],
+        'LIST_SECTION_FIELDS' => array('NAME'),
+        'LIST_ELEMENT_FIELDS' => array('NAME'),
+        'EDIT_GROUPS_EDIT_FIELDS' => array('NAME', 'GROUP'),
+        'EDIT_GROUPS_SHOW_FIELDS' => array('GROUP.NAME'),
+        'EDIT_ELEMENTS_EDIT_FIELDS' => array('NAME', 'GROUP', 'MEASURE'),
+        'EDIT_ELEMENTS_SHOW_FIELDS' => array('GROUP.NAME'),
+    )
 );?>
 
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
