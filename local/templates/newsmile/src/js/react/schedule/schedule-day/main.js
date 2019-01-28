077b93eaa1f032abad7ed35f1d42b80340306dbc
Alexander Bondarev <al94.bondarev@gmail.com>
2019-01-28 10:10:21 +0800

    #change переработка компонента расписания для отображения нескольких дней сразу

--- local/templates/newsmile/src/js/react/schedule/schedule-day/main.js
+++ local/templates/newsmile/src/js/react/schedule/schedule-day/main.js
@@ -7,12 +7,6 @@ import CookieHelper from 'js/helpers/cookie-helper';
 
 class ScheduleDay extends React.Component
 {
-    constructor(props)
-    {
-        super(props);
-        this.finalize = this.finalize.bind(this);
-    }
-
     getHalfTime(startTime, endTime)
     {
         let halfDiffSeconds = Math.round(endTime.diff(startTime) / 2000);
@@ -32,170 +26,13 @@ class ScheduleDay extends React.Component
         return moment(this.props.date + ' ' + time);
     }
 
-    getTimeLine(schedule, limits)
-    {
-        let timeLine = {};
-        let iterTime = limits.startTime.clone();
-        let endTime = limits.endTime;
-        let lastIntervalTime;
-
-        /*
-        сначала заполняем таймлайн стандартными интервалами в 30 минут, первый и последний интервал обрезаются до половинного
-        в зависимости от фильтра
-         */
-        while(iterTime.isBefore(endTime))
-        {
-            let timeDiff = (iterTime.get('minutes') % 30 === 15) ? 15 : 30;
-
-            let time = iterTime.format('HH:mm');
-            timeLine[time] = {
-                type: ((timeDiff === 15) ? 'half' : 'standard'),
-                height: null
-            };
-
-            iterTime.add(timeDiff, 'minutes');
-            lastIntervalTime = time
-        }
-
-        if(endTime.get('minutes') % 30 === 15)
-        {
-            // последний интервал - половинный, если конечное время из фильтра кратно 15 минутам
-            timeLine[lastIntervalTime].type = 'half';
-        }
-
-        /* затем по расписанию врачей (intervals) и приемам (visits) добавляем половинные интервалы (15 минут) */
-        schedule.forEach(chairSchedule =>
-        {
-            chairSchedule.intervals.concat(Object.values(chairSchedule.visits)).forEach(interval =>
-            {
-                let startTime = this.getMoment(interval.TIME_START);
-                if(startTime.get('minutes') % 30 === 15)
-                {
-                    startTime.add(-15, 'm');
-                    this.splitTimeLine(timeLine, startTime.format('HH:mm'));
-                }
-
-                let endTime = this.getMoment(interval.TIME_END);
-                if(endTime.get('minutes') % 30 === 15)
-                {
-                    endTime.add(-15, 'm');
-                    this.splitTimeLine(timeLine, endTime.format('HH:mm'));
-                }
-            });
-        });
-
-        return timeLine;
-    }
-
-    sortTimeLine(timeLine)
-    {
-        let sortedTimeLine = {};
-
-        Object.keys(timeLine).sort().forEach(time =>
-        {
-            sortedTimeLine[time] = timeLine[time];
-        });
-
-        return sortedTimeLine;
-    }
-
-    /**
-     * Делит определённый элемент таймлайна
-     * @param timeLine
-     * @param time - начальное время разделяемого элемента
-     */
-    splitTimeLine(timeLine, time)
-    {
-        if((time in timeLine) && (timeLine[time].type === 'standard'))
-        {
-            const nextTime = this.getMoment(time).add(15, 'm').format('HH:mm');
-            timeLine[nextTime] = {
-                type: 'half',
-                height: null
-            };
-
-            timeLine[time].type = 'half';
-        }
-    }
-
-    /**
-     * Вычисляет границы таймлайна. Если не указаны фильтры по врачу и специальности, в качестве границ таймлайна будут взяты границы фильтра
-     * по времени. Иначе верхней границей будет минимальное начальное время непустого незаблокированного (прошедшего по фильтру) интервала,
-     * а нижней границей - максимальное конечное время непустого незаблокированного интервала.
-     *
-     * @param schedule
-     * @returns {{startTime, endTime}}
-     */
-    getTimeLineLimits(schedule)
-    {
-        let startTime = this.getMoment(this.props.filter.timeFrom);
-        let endTime = this.getMoment(this.props.filter.timeTo);
-
-        if(this.props.filter.doctor || this.props.filter.specialization)
-        {
-            /* обрезаем таймлайн, чтобы убрать лишние пустые и блокированные ячейки по краям */
-
-            let minTimeStart = null;
-            let maxTimeEnd = null;
-
-            schedule.forEach(chairSchedule =>
-            {
-                let firstNotBlockedInterval = null;
-                let lastNotBlockedInterval = null;
-
-                chairSchedule.intervals.forEach(interval =>
-                {
-                    if(!this.isBlockedInterval(interval))
-                    {
-                        if(!firstNotBlockedInterval)
-                        {
-                            firstNotBlockedInterval = interval;
-                        }
-
-                        lastNotBlockedInterval = interval;
-                    }
-                });
-
-                if(firstNotBlockedInterval !== null)
-                {
-                    let firstTimeStart = this.getMoment(firstNotBlockedInterval.TIME_START);
-
-                    if((minTimeStart === null) || firstTimeStart.isBefore(minTimeStart))
-                    {
-                        minTimeStart = firstTimeStart;
-                    }
-                }
-
-                if(lastNotBlockedInterval !== null)
-                {
-                    let lastTimeEnd = this.getMoment(lastNotBlockedInterval.TIME_END);
-
-                    if((maxTimeEnd === null) || lastTimeEnd.isAfter(maxTimeEnd))
-                    {
-                        maxTimeEnd = lastTimeEnd;
-                    }
-                }
-            });
-
-            if(minTimeStart && startTime.isBefore(minTimeStart))
-            {
-                startTime = minTimeStart;
-            }
-
-            if(maxTimeEnd && endTime.isAfter(maxTimeEnd))
-            {
-                endTime = maxTimeEnd;
-            }
-        }
-
-        return {startTime, endTime};
-    }
 
     render()
     {
         this.doctors = this.getDoctors();
 
         const timeLineNode = React.createRef();
+        const timeLine = this.props.timeLine;
 
         /* ограничения расписания по времени - время начала и время окончания работы клиники, а также середина рабочего дня */
         let timeLimits = {
@@ -206,18 +43,16 @@ class ScheduleDay extends React.Component
         timeLimits.half = this.getHalfTime(timeLimits.start, timeLimits.end);
 
         /* подготовка производной информации по расписанию - массивов ячеек (cells) и id основных врачей (mainDoctors) */
-        const filter = this.props.filter(chairSchedule =>
+        const schedule = this.props.schedule.filter(chairSchedule =>
         {
-            this.addEmptyIntervals(chairSchedule.intervals, filter);
+            this.addEmptyIntervals(chairSchedule.intervals, this.props.filter);
             chairSchedule.cells = this.getCells(chairSchedule, timeLine);
             chairSchedule.mainDoctors = this.defineMainDoctors(chairSchedule.cells, timeLimits);
             this.defineHeight(timeLine, chairSchedule.cells, chairSchedule.mainDoctors);
 
-            return this.isEmptyCells(chairSchedule.cells);
+            return !this.isEmptyCells(chairSchedule.cells);
         });
         /* */
-
-        const timeLine = this.props.timeLine;
 
         return (
             <div className="dayCalendar_cont" onContextMenu={this.blockEvent}>
@@ -293,90 +128,6 @@ class ScheduleDay extends React.Component
         );
     }
 
-    /**
-     * Фильтрует расписание - отбрасывает интервалы, которые не подходят по времени (ограничения по времени принимает в аргументе timeLineLimits),
-     * интервалы на границе при необходимости делит пополам (если в фильтре по времени указаны 15 или 45 минут). Помечает заблокированные интервалы
-     * @param schedule
-     * @param timeLineLimits
-     * @returns {*}
-     */
-    filterSchedule(schedule, timeLineLimits)
-    {
-        let timeFrom = timeLineLimits.startTime;
-        let timeTo = timeLineLimits.endTime;
-
-        schedule.forEach(chairSchedule =>
-        {
-            let intervals = [];
-            let visits = {};
-
-            chairSchedule.intervals.concat(Object.values(chairSchedule.visits)).forEach(interval =>
-            {
-                /* фильтрация по времени */
-
-                let intervalStartTime = this.getMoment(interval.TIME_START);
-                let intervalEndTime = this.getMoment(interval.TIME_END);
-                let isSuitable = true;
-                let isVisit = !!interval.STATUS;
-
-                /**
-                 * время, которое используется для выбора позиции в таблице
-                 */
-                let shownTimeStart = interval.TIME_START;
-
-                if(intervalStartTime.isBefore(timeFrom))
-                {
-
-                    if(intervalEndTime.isAfter(timeFrom))
-                    {
-                        // значит начальным временем из фильтра распилили интервал на части, нужно подменить начальное время интервала
-                        shownTimeStart = timeFrom.format('HH:mm');
-                    }
-                    else
-                    {
-                        isSuitable = false;
-                    }
-                }
-
-                if(intervalEndTime.isAfter(timeTo))
-                {
-                    if(intervalStartTime.isBefore(timeTo))
-                    {
-                        if(!isVisit)
-                        {
-                            // значит конечным временем из фильтра распилили интервал на части, нужно подменить конечное время интервала
-                            interval.TIME_END = timeTo.format('HH:mm');
-                        }
-                    }
-                    else
-                    {
-                        isSuitable = false;
-                    }
-                }
-
-                if(isSuitable)
-                {
-                    /* вычисляем, блокирован ли интервал (фильтрация по врачу и специальности) */
-
-                    interval.isBlocked = this.isBlockedInterval(interval);
-                    if(isVisit)
-                    {
-                        visits[shownTimeStart] = interval;
-                    }
-                    else
-                    {
-                        interval.TIME_START = shownTimeStart;
-                        intervals.push(interval);
-                    }
-                }
-            });
-
-            chairSchedule.intervals = intervals;
-            chairSchedule.visits = visits;
-        });
-
-        return schedule;
-    }
 
     /**
      * Проверяет, является ли массив ячеек пустым (не содержит ячеек вообще, либо содержит ячейки с неназначенными врачами,
@@ -400,22 +151,6 @@ class ScheduleDay extends React.Component
         return isEmpty;
     }
 
-    /**
-     * Проверяет, является ли интервал заблокированным. Заблокированный интервал - это интервал, не подходящий под фильтр по врачу,
-     * либо по специальности
-     * @param interval
-     * @returns {boolean}
-     */
-    isBlockedInterval(interval)
-    {
-        let doctor = this.doctors[interval.DOCTOR_ID];
-        if(!doctor || !Object.keys(doctor).length) return false;
-
-        let isBlockedByDoctor = !!this.props.filter.doctor && (this.props.filter.doctor !== interval.DOCTOR_ID);
-        let isBlockedBySpec = !!this.props.filter.specialization && (this.props.filter.specialization !== doctor.specialization_code);
-
-        return  !!interval.DOCTOR_ID && (isBlockedByDoctor || isBlockedBySpec);
-    }
 
     /**
      * Получает объект врачей из массива (указывает id врачей в качестве ключей)
@@ -556,6 +291,8 @@ class ScheduleDay extends React.Component
             resultWithVisits[time] = cell;
         }
 
+        console.log(resultWithVisits);
+
         return resultWithVisits;
     }
 
@@ -585,33 +322,6 @@ class ScheduleDay extends React.Component
         }
     }
 
-    splitInterval(time)
-    {
-        let splittedTime = GeneralHelper.clone(this.state.splittedTime);
-        if(!splittedTime[this.props.date])
-        {
-            splittedTime[this.props.date] = [];
-        }
-
-        splittedTime[this.props.date].push(time);
-        this.setState({splittedTime});
-    }
-
-    /**
-     * Получает массив времени, которое можно объединить
-     * @param timeLine
-     * @returns {*}
-     */
-    getAvailableTimeUnite(timeLine)
-    {
-        let result = this.state.splittedTime[this.props.date];
-        if(!result) return [];
-
-        return result.filter(splittedTime =>
-        {
-            return (splittedTime in timeLine) && (timeLine[splittedTime].type === 'standard');
-        })
-    }
 
     /**
      * Возвращает массив mainDoctors (основных врачей) для указанного массива ячеек.
