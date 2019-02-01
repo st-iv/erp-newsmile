ce8a141b2342826f3124be4f15293a237e48fb6b
Alexander Bondarev <al94.bondarev@gmail.com>
2019-02-01 18:27:25 +0800

    #add вывод документации по API

---
+++ local/modules/mmit.newsmile/lib/command/service/descriptor.php
@@ -0,0 +1,14 @@
+<?
+
+
+namespace Mmit\NewSmile\Command\Service;
+
+use Mmit\NewSmile\Command\EntityDescriptor;
+
+class Descriptor extends EntityDescriptor
+{
+    public function getDescription()
+    {
+        return 'Услуги клиники';
+    }
+}
