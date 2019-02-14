4aed30fcfe7db27eb63bd798c0b192f9322a93f0
st-iv <stepan.ivanov93@gmail.com>
2019-02-14 15:42:51 +0800

    #fix navAppt

--- local/templates/newsmile/erp_frontend/src/App.js
+++ local/templates/newsmile/erp_frontend/src/App.js
@@ -4,10 +4,26 @@ import Sidebar from './components/Sidebar/Sidebar'
 import Header from './components/Header/Header'
 import Schedule from './components/Schedule/Schedule'
 import Appt from './components/Appt/Appt'
+import ServerCommand from './common/server/server-command'
 // import Test from './components/Test/Test'
 
 
 class App extends Component {
+
+    state = {
+        calendar: null,
+        doctors: null,
+        initialDate: null,
+        notices: null,
+        schedule: null,
+        search: null
+    };
+
+    componentWillMount() {
+        var command = new ServerCommand('general/get-index-data');
+        command.exec().then(response => this.setState(response));
+    }
+
     render() {
         return (
             <div className="App">
@@ -15,7 +31,9 @@ class App extends Component {
                 <div className="main-content">
                     <Header />
                     <Appt />
-                    {/*<Schedule calendar={this.props.calendar} doctors={this.props.doctors} initialDate={this.props.initialDate} schedule={this.props.schedule} />*/}
+                    {this.state.schedule && (
+                        <Schedule calendar={this.state.calendar} doctors={this.state.doctors} initialDate={this.state.initialDate} schedule={this.state.schedule} />
+                    )}
                 </div>
             </div>
         );
--- local/templates/newsmile/erp_frontend/src/components/Appt/NavAppt/NavAppt.js
+++ local/templates/newsmile/erp_frontend/src/components/Appt/NavAppt/NavAppt.js
@@ -1,32 +1,63 @@
 import React from 'react'
 import './NavAppt.scss'
 
+const stepNames = [
+    {
+        id: 1,
+        text: 'Первичный осмотр'
+    },
+    {
+        id: 2,
+        text: 'План лечения'
+    },
+    {
+        id: 3,
+        text: 'Процесс лечения'
+    },
+    {
+        id: 4,
+        text: 'Наряд'
+    },
+    {
+        id: 5,
+        text: 'Курс лечения'
+    },
+    {
+        id: 6,
+        text: 'Гарантии'
+    },
+    {
+        id: 7,
+        text: 'V'
+    }
+
+]
+
+class ApptStep extends React.Component {
+    render() {
+        const stepsTemplate = this.props.data.map(function (item, index) {
+            return (
+                <div key={index}>
+                    <div className="appt-nav__item">
+                        {item.text}
+                    </div>
+                </div>
+            )
+        })
+
+        return (
+            <div className="appt-nav">
+                {stepsTemplate}
+            </div>
+        )
+
+    }
+}
+
 export default class HeaderAppt extends React.Component {
     render() {
         return (
-            <div className="appt-nav">
-                <div className="appt-nav__item appt-nav__item--active">
-                    Первичный осмотр
-                </div>
-                <div className="appt-nav__item">
-                    План лечения
-                </div>
-                <div className="appt-nav__item">
-                    Процесс лечения
-                </div>
-                <div className="appt-nav__item">
-                    Наряд
-                </div>
-                <div className="appt-nav__item">
-                    Курс лечения
-                </div>
-                <div className="appt-nav__item">
-                    Гарантии
-                </div>
-                <div className="appt-nav__item">
-                    V
-                </div>
-            </div>
+            <ApptStep data={stepNames}/>
         )
     }
 }
