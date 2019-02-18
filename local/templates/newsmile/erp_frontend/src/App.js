ab2e42c23b432ecb0a618fc761fe276452cadd3f
“Yulya <yeellowfish@gmail.com>
2019-02-18 15:12:56 +0800

    #fix styles for HeaderAppt and NavAppt and add Notifications

--- local/templates/newsmile/erp_frontend/src/App.js
+++ local/templates/newsmile/erp_frontend/src/App.js
@@ -4,6 +4,7 @@ import Sidebar from './components/Sidebar/Sidebar'
 import Header from './components/Header/Header'
 import Schedule from './components/Schedule/Schedule'
 import Appt from './components/Appt/Appt'
+import Notifications from './components/common/Notifications/Notifications'
 import ServerCommand from './common/server/server-command'
 // import Test from './components/Test/Test'
 
@@ -27,6 +28,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <Notifications />
                 <Sidebar />
                 <div className="main-content">
                     <Header />
