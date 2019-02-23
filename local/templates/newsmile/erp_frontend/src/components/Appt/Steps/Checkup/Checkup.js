136d61b29c5d200c6e11cd0e830b1ec665a8bd1e
stiv <stepan.ivanov93@gmail.com>
2019-02-24 02:52:19 +0800

    переход по шагам без перезагрузки страницы

--- local/templates/newsmile/erp_frontend/src/components/Appt/Steps/Checkup/Checkup.js
+++ local/templates/newsmile/erp_frontend/src/components/Appt/Steps/Checkup/Checkup.js
@@ -4,46 +4,36 @@ import Button from "../../../common/Button/Button"
 import {IconPrint} from "./../../../common/Icons"
 import CheckupForm from './CheckupForm'
 import ToothCard from './ToothCard'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, NavLink} from 'react-router-dom'
 
 export default class Checkup extends React.Component {
     render() {
         return (
-            <div className="checkup">
-                <CheckupHeader />
                 <BrowserRouter>
-                    <div className="cont">
+                    <div className="checkup">
+                    <React.Fragment>
+                        <div className="checkup-header">
+                            <div className="checkup-header__desc">
+                                Детально опишите осмотр пациента
+                            </div>
+                            <div className="checkup-steps">
+                                <NavLink to="/tooth-card" className="checkup-steps__item">
+                                    Зубная карта
+                                </NavLink>
+                                <NavLink to="/checkup-form" className="checkup-steps__item">
+                                    Осмотр
+                                </NavLink>
+                                <NavLink to="/print" className="checkup-steps__item">
+                                    <IconPrint width="15" height="18"/>
+                                </NavLink>
+                            </div>
+                            <Button variant="success" text="Сохранить и продолжить" action="next"/>
+                        </div>
+                    </React.Fragment>
                         <Route path="/tooth-card" component={ToothCard}/>
                         <Route path="/checkup-form" component={CheckupForm}/>
                     </div>
                 </BrowserRouter>
-            </div>
-        )
-    }
-}
-
-class CheckupHeader extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div className="checkup-header">
-                    <div className="checkup-header__desc">
-                        Детально опишите осмотр пациента
-                    </div>
-                    <div className="checkup-steps">
-                        <a href="/tooth-card" className="checkup-steps__item">
-                           Зубная карта
-                        </a>
-                        <a href="/checkup-form" className="checkup-steps__item">
-                            Осмотр
-                        </a>
-                        <a href="/print" className="checkup-steps__item">
-                            <IconPrint width="15" height="18"/>
-                        </a>
-                    </div>
-                    <Button variant="success" text="Сохранить и продолжить" action="next"/>
-                </div>
-            </React.Fragment>
         )
     }
 }
