fd510898bdce293c1a12abf96cf1969b5d58c2f2
st-iv <stepan.ivanov93@gmail.com>
2019-02-26 10:46:56 +0800

    test

--- local/templates/newsmile/erp_frontend/src/components/Test/Test.js
+++ local/templates/newsmile/erp_frontend/src/components/Test/Test.js
@@ -2,48 +2,27 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Test.scss'
 
-const Star =({selected=false, onClick=f=>f}) => {
+const TodoList = (props) => {
     return (
-        <div className={(selected)? "star selected" : "star"} onClick={onClick}>
-        </div>
+        <ul>
+            <li><TodoListItem /></li>
+            <li><TodoListItem /></li>
+        </ul>
     )
 }
 
-Star.propTypes = {
-    selected: PropTypes.bool,
-    onClick: PropTypes.func,
+const TodoListItem = () => {
+    return (
+        <span>Drink Coffee</span>
+    )
 }
 
-class StarRating extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            starsSelected: 0
-        }
-        this.change = this.change.bind(this)
-    }
-
-    change(starsSelected) {
-        this.setState({starsSelected})
-    }
-
-    render() {
-        const {totalStars} = this.props
-        const {starSelected} = this.state
-        return (
-            <div className="star-rating">
-
-            </div>
-        )
-    }
-}
 export default class Test extends React.Component {
 
     render() {
         return (
             <div>
-                <span>rating</span>
-                <Star></Star>
+                <TodoList />
             </div>
         )
     }
